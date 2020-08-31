ea03476ce5af1ec21fcc98ead88e8f550643a408
Jungjaedev <jungjaedev@gmail.com>
2020-08-31 13:17:24 +0900

    Multiplication table

--- C lan/정해진 개수의 정수를 더하는 프로그램.c
+++ C lan/정해진 개수의 정수를 더하는 프로그램.c
@@ -2,5 +2,15 @@
 
 int main(void)
 {
+	int number, x, i, sum = 0;
+	printf("정수의 개수를 입력하세요 : ");
+	scanf("%d", &number);
+	for(i = 0; i < number; i++)
+	{
+		printf("정수의 값을 입력하세요 : ");
+		scanf("%d", &x);
+		sum += x;
+	}
+	printf("전체 정수의 값은 %d입니다.\n",sum);
 	return 0;
 }
