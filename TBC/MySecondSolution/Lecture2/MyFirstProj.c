432ce04ed9d3341dd9482d67a7c9926eba94d62b
Jungjaedev <jungjaedev@gmail.com>
2020-12-19 22:07:39 +0900

    TBC

---
+++ TBC/MySecondSolution/Lecture2/MyFirstProj.c
@@ -0,0 +1,8 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	
+
+	return 0;
+}
---
+++ TBC/MySecondSolution/MySecondSolution/MyFirstProj.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+
+void say_hello(void)
+{
+	printf("Hello, World!");
+}
+int main()
+{
+	int x, y, z;
+
+	x = 1;
+	y = 20;
+	z = 3;
+
+	z = x + y;
+
+	say_hello();
+
+	return 0;
+}
