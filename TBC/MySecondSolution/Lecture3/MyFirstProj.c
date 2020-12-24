cb69fdd06e3ec7f1b6c90b7a15a1e14a46955310
Jungjaedev <jungjaedev@gmail.com>
2020-12-24 15:24:22 +0900

    TBC

--- TBC/MySecondSolution/Lecture3/MyFirstProj.c
+++ TBC/MySecondSolution/Lecture3/MyFirstProj.c
@@ -1,11 +1,19 @@
-﻿#include <stdio.h>
+﻿#include <limits.h>
+#include <stdio.h>
 
-int main()
-{
-	unsigned int i = 0;
+int main() {
+	unsigned int u_max = UINT_MAX;
+	unsigned int u_min = 0;
+	signed int i_max = INT_MAX;
+	signed int i_min = INT_MIN;
 
-	sizeof(unsigned int);
 
+	printf("max of uint = %u\n", u_max);
+	printf("min of uint = %u\n", u_min);
+
+	printf("max of int = %d\n", i_max);
+	printf("min of int = %d\n", i_min);
 
 	return 0;
 }
+
