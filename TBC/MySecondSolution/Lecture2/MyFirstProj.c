3a6e0410d5a4e0019f65b36e43dd4179b80733ea
Jungjaedev <jungjaedev@gmail.com>
2021-01-08 22:42:32 +0900

    strnstr

--- TBC/MySecondSolution/Lecture2/MyFirstProj.c
+++ TBC/MySecondSolution/Lecture2/MyFirstProj.c
@@ -1,16 +1,36 @@
 ﻿#include <stdio.h>
-#include <string.h>
 
-#define SIZE 40
 
-int main(void)
+char* ft_strnstr(const char* big, const char* little, size_t len)
 {
-    char buffer1[SIZE] = "computer program";
-    char* ptr;
-    int    ch = 'p';
+	size_t	i;
+	size_t	j;
 
-    ptr = strchr(buffer1, ch);
-    printf("The first occurrence of %c in '%s' is '%s'\n",
-        ch, buffer1, ptr);
+	i = 0;
+	j = 0;
+	if (*little == '\0')
+		return ((char*) big);
+	while (big[i] != '\0' && i < len)
+	{
+		while (big[i + j] == little[j] && i + j < len)
+		{
+			j++;
+			if (little[j] == '\0')
+				return ((char*)&big[i]);
+		}
+		i++;
+	}
+	return (0);
+}
 
+int main()
+{
+	char s1[30] = "pen pineapple apple pen!";
+
+	char* ptr = ft_strnstr(s1, "pine", 10);
+	printf("%s\n", ptr);
+	char* ptr1 = ft_strnstr(s1, "pine", 5);
+	printf("%s\n", ptr1);
+
+	return (0);
 }
