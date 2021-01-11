75facdfdeb268553126c39b33e767296fa16da57
Jungjaedev <jungjaedev@gmail.com>
2021-01-11 14:49:31 +0900

    is ft

--- TBC/MySecondSolution/Lecture2/MyFirstProj.c
+++ TBC/MySecondSolution/Lecture2/MyFirstProj.c
@@ -1,52 +1,22 @@
 ﻿#include <stdio.h>
+//#include <ctype.h>
 
-/*
-int	ft_strncmp(const char* s1, const char* s2, size_t n)
+int	ft_isascii(int c)
 {
-	size_t	i;
-
-	i = 0;
-	if (!n)
-		return (0);
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < (n - 1))
-		i++;
-	if (s1[i] == s2[i])
-		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}*/
-
-int					ft_strncmp(const char* s1, const char* s2, size_t n)
-{
-	unsigned char* p1;
-	unsigned char* p2;
-	size_t			i;
-
-	p1 = (unsigned char*)s1;
-	p2 = (unsigned char*)s2;
-	i = 0;
-	while (n--)
-	{
-		if (p1[i] != p2[i] || p1[i] == 0 || p2[i] == 0)
-			return (p1[i] - p2[i]);
-		i++;
-	}
-	return (0);
+    return (c >= 0 && c <= 127);
 }
 
-int main()
+void main(void)
 {
-    char s1[10] = "aaa";
-    char s2[10] = "aab";
-
-    int compare1 = ft_strncmp(s1, s2, 2);
-    int compare2 = ft_strncmp(s2, s1, 4);
-    int compare3 = ft_strncmp(s1, s2, 4);
-    int compare4 = ft_strncmp(s1, s2, 0);
-
-    printf("결과1 : %d\n", compare1);
-    printf("결과2 : %d\n", compare2);
-    printf("결과3 : %d\n", compare3);
-    printf("결과4 : %d\n", compare4);
-
-    return 0;
+    char ch;
+    printf("한 문자를 입력해 주세요 : ");
+    scanf("%c", &ch);
+    if (ft_isascii(ch) != 0)
+    {
+        printf("ascii 문자입니다.\n");
+    }
+    else
+    {
+        printf("ascii 문자가 아닙니다.\n");
+    }
 }
